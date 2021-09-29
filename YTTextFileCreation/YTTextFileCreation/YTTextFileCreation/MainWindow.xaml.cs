e70d9167d41286756560670e90d56b0e5db2a46d
BANDA Ranjit <ranjit.banda@hexagon.com>
2021-09-29 14:55:27 +0530

    YTFileCreation

---
+++ YTTextFileCreation/YTTextFileCreation/YTTextFileCreation/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace YTTextFileCreation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string lstrDesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string lstrFile = string.Concat(lstrDesktopPath, "\\", txtTextFileName.Text);
+            bool test = int.TryParse(txtCount.Text, out int lintCount);
+            List<string> URLs = new List<string>();
+            for (int i = 0; i < lintCount; i++)
+            {
+                URLs.Add(txtURL.Text);
+            }
+            File.WriteAllLinesAsync(lstrFile, URLs);
+        }
+    }
+}
