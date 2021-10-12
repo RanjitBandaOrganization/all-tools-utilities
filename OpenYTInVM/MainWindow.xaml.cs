0c7abdffed932aae2bbb3c7a423900174bd1dee7
BANDA Ranjit <ranjit.banda@hexagon.com>
2021-10-12 19:20:53 +0530

    Seems to work all the functionalities

--- OpenYTInVM/MainWindow.xaml.cs
+++ OpenYTInVM/MainWindow.xaml.cs
@@ -74,11 +74,31 @@ namespace OpenYTInVM
 
         private void OpenYoutubeURLsAfterevery15secs()
         {
+            int XPOS, YPOS = 0;
+            
             youtubeURLsToOpen.Clear();
+            //
+            //RANJIT - Logic - This is to open Mofiki's Coordinate Finder.exe, so that we can find the coordinates of the play button
+            //
+            ProcessStartInfo processStartInfo1 = new ProcessStartInfo(@"C:\Users\admindemo\Desktop\Mofiki's Coordinate Finder.exe");
+            Process.Start(processStartInfo1);
+            //
+            //RANJIT - Logic - This is to open config file in the remote system so that it is easy to modify the configuration values
+            //
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("notepad.exe");
+            processStartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            processStartInfo.Arguments = @"C:\Users\admindemo\Desktop\1_OpenYTInVM.exe.config";
+            Process.Start(processStartInfo);
+            
 
-            string txtWaitForEachURLTillURLisComplete, txtTimeDelayAfterOpeningURLinSeconds, txtNumberOfTimeToLoopWatching, txtNumberOfTimesToOpenURLAtOnce, txtShutdownAfterAllWorkIsDone, txtKillBrowserForEachURL, txtKillBrowserFirstTimeBeforeLoading, txtMuteSystem, txtAllowMediaAutoPlay, txtDisableMediaAutoPlay, txtTimeDelayBeforeClosingURLAfterCompletioninSeconds;
-            ExtractAppConfigurationValues(out txtWaitForEachURLTillURLisComplete, out txtTimeDelayAfterOpeningURLinSeconds, out txtNumberOfTimeToLoopWatching, out txtNumberOfTimesToOpenURLAtOnce, out txtShutdownAfterAllWorkIsDone, out txtKillBrowserForEachURL, out txtKillBrowserFirstTimeBeforeLoading, out txtMuteSystem, out txtAllowMediaAutoPlay, out txtDisableMediaAutoPlay, out txtTimeDelayBeforeClosingURLAfterCompletioninSeconds);
+            string txtWaitForEachURLTillURLisComplete, txtTimeDelayAfterOpeningURLinSeconds, txtNumberOfTimeToLoopWatching, txtNumberOfTimesToOpenURLAtOnce, txtShutdownAfterAllWorkIsDone, txtKillBrowserForEachURL, txtKillBrowserFirstTimeBeforeLoading, txtMuteSystem, txtAllowMediaAutoPlay, txtDisableMediaAutoPlay, txtTimeDelayBeforeClosingURLAfterCompletioninSeconds, txtMouseClick, txtXPOS, txtYPOS;
+            ExtractAppConfigurationValues(out txtWaitForEachURLTillURLisComplete, out txtTimeDelayAfterOpeningURLinSeconds, out txtNumberOfTimeToLoopWatching, out txtNumberOfTimesToOpenURLAtOnce, out txtShutdownAfterAllWorkIsDone, out txtKillBrowserForEachURL, out txtKillBrowserFirstTimeBeforeLoading, out txtMuteSystem, out txtAllowMediaAutoPlay, out txtDisableMediaAutoPlay, out txtTimeDelayBeforeClosingURLAfterCompletioninSeconds, out txtMouseClick, out txtXPOS, out txtYPOS);
 
+            //
+            //RANJIT - Logic - This is to ALLOW / DISABLE - Media Auto Play option in the Edge
+            //  NOTE - Once the YOUTUBE video is running then this option will not have any significance on the subsequence videos being opened in the new tab
+            //              As they keep playing in the new tab though if you HAD DISABLED AUTO MEDIA PLAY as already video is being played in the previous tab page
+            //
             if (string.Compare(txtAllowMediaAutoPlay.ToUpper(), "TRUE") == 0)
             {
                 //THIS SEEMS TO BE AN ISSUE WITH AUTOMATIC PLAY OPTION.   LETS INVESTIGATE FURTHER
@@ -100,38 +120,56 @@ namespace OpenYTInVM
 
             ProcessCopy processCopy = new ProcessCopy();
             List<CopyDetails> copyDetailsList = processCopy.ReadCSVAndPopulateList(".\\HelperFiles\\YT_URLs.csv");
-            
 
+            //
+            //RANJIT - Logic - This is to mute the system.   Currently i am setting volume 1
+            //
             if (string.Compare(txtMuteSystem.ToUpper(), "TRUE") == 0)
             {
                 //Mute all audio devices
-                SetVolume(0);
+                SetVolume(1);
             }
-            _ = int.TryParse(txtNumberOfTimeToLoopWatching, out int lintNumberOfTimeToLoopWatching);
 
+            //
+            //RANJIT - Logic - This is the number of times to loop all the URLs from YT_URLs list
+            //
+            _ = int.TryParse(txtNumberOfTimeToLoopWatching, out int lintNumberOfTimeToLoopWatching);
             for (int i = 0; i < lintNumberOfTimeToLoopWatching; i++)
             {
                 foreach (var item in copyDetailsList)
                 {
-                    //Logging
+                    //
+                    //RANJIT - Logic - Logging the URL being played
+                    //
                     AppendTextToFile("------------------------");
                     AppendTextToFile(item.Title);
                     AppendTextToFile(item.URL);
                     AppendTextToFile(item.Time);
                     AppendTextToFile("------------------------");
 
-                    ExtractAppConfigurationValues(out txtWaitForEachURLTillURLisComplete, out txtTimeDelayAfterOpeningURLinSeconds, out txtNumberOfTimeToLoopWatching, out txtNumberOfTimesToOpenURLAtOnce, out txtShutdownAfterAllWorkIsDone, out txtKillBrowserForEachURL, out txtKillBrowserFirstTimeBeforeLoading, out txtMuteSystem, out txtAllowMediaAutoPlay, out txtDisableMediaAutoPlay, out txtTimeDelayBeforeClosingURLAfterCompletioninSeconds);
+                    //
+                    //RANJIT - Logic - Extract cofiguration values for each URL being opened so that config values modified in between the URLs open will take effect.
+                    //
+                    ExtractAppConfigurationValues(out txtWaitForEachURLTillURLisComplete, out txtTimeDelayAfterOpeningURLinSeconds, out txtNumberOfTimeToLoopWatching, out txtNumberOfTimesToOpenURLAtOnce, out txtShutdownAfterAllWorkIsDone, out txtKillBrowserForEachURL, out txtKillBrowserFirstTimeBeforeLoading, out txtMuteSystem, out txtAllowMediaAutoPlay, out txtDisableMediaAutoPlay, out txtTimeDelayBeforeClosingURLAfterCompletioninSeconds, out txtMouseClick, out txtXPOS, out txtYPOS);
                     _ = int.TryParse(txtTimeDelayAfterOpeningURLinSeconds, out int lintTimeDelayAfterOpeningURLinSeconds);
 
-                    //Process.Start(string.Concat("microsoft-edge:", item.URL));
+                    //
+                    //RANJIT - Logic - Number of times to open the same URL simultaneously
+                    //
                     _ = int.TryParse(txtNumberOfTimesToOpenURLAtOnce, out int lintNumberOfTimesToOpenURLAtOnce);
                     for (int y = 0; y < lintNumberOfTimesToOpenURLAtOnce; y++)
                     {
+
+                        //
+                        //RANJIT - Logic - This is to kill the edge browser to skip SETUP steps while opening EDGE browser first time in new system
+                        //
                         if (mblnFirstTimeLaunchingTheBrowser && (string.Compare(txtKillBrowserFirstTimeBeforeLoading.ToUpper(), "TRUE") == 0))
                         {
                             for (int z = 0; z < 2; z++)
                             {
-                                System.Diagnostics.Process.Start("https://www.youtube.com");
+                                System.Diagnostics.Process.Start("https://www.youtube.com"); 
+                                
+                                
                                 System.Threading.Thread.Sleep(lintTimeDelayAfterOpeningURLinSeconds * 1000);
                                 KillEdgeBrowser();
                                 System.Threading.Thread.Sleep(lintTimeDelayAfterOpeningURLinSeconds * 1000);
@@ -142,12 +180,34 @@ namespace OpenYTInVM
                         {
                             mblnFirstTimeLaunchingTheBrowser = false;
                         }
+
+                        //
+                        //RANJIT - Logic - MAIN - URL that will be opened and played
+                        //
                         System.Diagnostics.Process.Start(item.URL);
                         System.Threading.Thread.Sleep(lintTimeDelayAfterOpeningURLinSeconds * 1000);
 
-                        LeftMouseClick(384, 384);
+                        //
+                        //RANJIT - Logic - MAIN - Simulate USER clicks
+                        //  NOTE - for this MOUSE CLICK to work, i assume the remote session must be in focus.
+                        //
+                        _ = int.TryParse(txtXPOS, out XPOS);
+                        _ = int.TryParse(txtYPOS, out YPOS);
+                        if (string.Compare(txtMouseClick.ToUpper(), "TRUE") == 0)
+                        {
+                            LeftMouseClick(XPOS, YPOS);
+                        }
+                        else
+                        {
+                            LeftMouseClick(XPOS, YPOS);
+                            System.Threading.Thread.Sleep(2 * 1000);
+                            LeftMouseClick(XPOS, YPOS);
+                        }
                     }
 
+                    //
+                    //RANJIT - Logic - Wait for each URL is complete before opening the next URL
+                    //
                     if (string.Compare(txtWaitForEachURLTillURLisComplete.ToUpper(), "TRUE") == 0)
                     {
                         List<string> llstTimeComponents = item.Time.Split(':').ToList();
@@ -158,9 +218,12 @@ namespace OpenYTInVM
                         int lintTotalTimeinSeconds = lintTimeInMins * 60 + lintTimeInSeconds + lintTimeDelayBeforeClosingURLAfterCompletioninSeconds;
                         System.Threading.Thread.Sleep(lintTotalTimeinSeconds * 1000);
                     }
-                    //_ = int.TryParse(txtTimeDelayAfterOpeningURLinSeconds, out int lintTimeDelayAfterOpeningURLinSeconds);
+                    
                     System.Threading.Thread.Sleep(lintTimeDelayAfterOpeningURLinSeconds * 1000);
 
+                    //
+                    //RANJIT - Logic - This is to find out what is the process name of the edge browser.   May be useful in the future if they processname changes
+                    //
                     //////Process[] edgeProcessListFull = Process.GetProcesses();
                     //////List<string> llstProcessNames = new List<string>();
                     //////foreach (var item2 in edgeProcessListFull)
@@ -171,6 +234,9 @@ namespace OpenYTInVM
                     //////    }
                     //////}
 
+                    //
+                    //RANJIT - Logic - Kill all the instances of the edge browser
+                    //
                     if (string.Compare(txtKillBrowserForEachURL.ToUpper(), "TRUE") == 0)
                     {
                         KillEdgeBrowser();
@@ -179,8 +245,23 @@ namespace OpenYTInVM
                 }
             }
 
+            //
+            //RANJIT - Logic - Shutdown the system
+            //
             if (string.Compare(txtShutdownAfterAllWorkIsDone.ToUpper(), "TRUE") == 0)
             {
+                //
+                //RANJIT - Logic - If wait for each URL is complete is FALSE then we are waiting 20 minutes.  So that all the URLs can be watched
+                //
+                if (string.Compare(txtWaitForEachURLTillURLisComplete.ToUpper(), "TRUE") == 0)
+                {
+                    System.Threading.Thread.Sleep(1 * 1000);
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(20 * 60 * 1000);
+                }
+
                 var psi = new ProcessStartInfo("shutdown", "/s /t 0");
                 psi.CreateNoWindow = true;
                 psi.UseShellExecute = false;
@@ -189,7 +270,7 @@ namespace OpenYTInVM
 
         }
 
-        private static void ExtractAppConfigurationValues(out string txtWaitForEachURLTillURLisComplete, out string txtTimeDelayAfterOpeningURLinSeconds, out string txtNumberOfTimeToLoopWatching, out string txtNumberOfTimesToOpenURLAtOnce, out string txtShutdownAfterAllWorkIsDone, out string txtKillBrowserForEachURL, out string txtKillBrowserFirstTimeBeforeLoading, out string txtMuteSystem, out string txtAllowMediaAutoPlay, out string txtDisableMediaAutoPlay, out string txtTimeDelayBeforeClosingURLAfterCompletioninSeconds)
+        private static void ExtractAppConfigurationValues(out string txtWaitForEachURLTillURLisComplete, out string txtTimeDelayAfterOpeningURLinSeconds, out string txtNumberOfTimeToLoopWatching, out string txtNumberOfTimesToOpenURLAtOnce, out string txtShutdownAfterAllWorkIsDone, out string txtKillBrowserForEachURL, out string txtKillBrowserFirstTimeBeforeLoading, out string txtMuteSystem, out string txtAllowMediaAutoPlay, out string txtDisableMediaAutoPlay, out string txtTimeDelayBeforeClosingURLAfterCompletioninSeconds, out string txtMouseClick, out string txtXPOS, out string txtYPOS)
         {
             txtWaitForEachURLTillURLisComplete = ConfigurationManager.AppSettings["WaitForEachURLTillURLisComplete"];
             txtTimeDelayAfterOpeningURLinSeconds = ConfigurationManager.AppSettings["TimeDelayAfterOpeningURLinSeconds"];
@@ -202,6 +283,10 @@ namespace OpenYTInVM
             txtAllowMediaAutoPlay = ConfigurationManager.AppSettings["AllowMediaAutoPlay"];
             txtDisableMediaAutoPlay = ConfigurationManager.AppSettings["DisableMediaAutoPlay"];
             txtTimeDelayBeforeClosingURLAfterCompletioninSeconds = ConfigurationManager.AppSettings["TimeDelayBeforeClosingURLAfterCompletioninSeconds"];
+            txtMouseClick = ConfigurationManager.AppSettings["MouseClick"];
+            txtXPOS = ConfigurationManager.AppSettings["XPOS"];
+            txtYPOS = ConfigurationManager.AppSettings["YPOS"];
+            
         }
 
         private static void KillEdgeBrowser()
