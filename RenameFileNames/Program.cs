d444ee73e0ba653a30e6a8483b2d7f65158e1011
BANDA Ranjit <ranjit.banda@hexagon.com>
2022-12-22 16:15:58 +0530

    Added rename file names solution

---
+++ RenameFileNames/Program.cs
@@ -0,0 +1,16 @@
+﻿// Get list of files in the specific directory.
+// ... Please change the first argument.
+string[] files = Directory.GetFiles(@"C:\Users\rkbanda\Desktop\PART 1 - Copy\",
+    "*.*",
+    SearchOption.AllDirectories); ;
+
+
+
+// Display all the files.
+foreach (string file in files)
+{
+    FileInfo lobjFile = new FileInfo(file);
+
+    string newfilename = string.Concat(lobjFile.Name.Substring(0, 5), "00", lobjFile.Name.Substring(5));
+    lobjFile.MoveTo(Path.Combine(lobjFile.DirectoryName, newfilename));
+}
